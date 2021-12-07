b2b1d8a3a50f2e9b111d2efab51f3b0754be25c7
Denis Snezhko <mikle4421@mail.ru>
2021-12-07 18:43:03 +0300

    Create the project.

---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ServerProject
+//
+//  Created by Denis Snezhko on 06.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
